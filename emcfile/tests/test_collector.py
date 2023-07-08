8c47977566a71a73d636f1be39ed353065d1457d
szsdk <shenz34206@hotmail.com>
2023-07-08 07:20:59 +0200

    add: write to PatternsSOneCollector

--- emcfile/tests/test_collector.py
+++ emcfile/tests/test_collector.py
@@ -4,14 +4,20 @@ import pytest
 import emcfile as ef
 
 
-def test_collector():
+def test_collector(tmp_path):
     cl = ef.PatternsSOneCollector(16)
     imgs = []
     for _ in range(30):
         img = np.random.randint(32, size=34)
         cl.append(img)
         imgs.append(img)
-    assert ef.patterns(np.array(imgs)) == cl.patterns()
+    ref = ef.patterns(np.array(imgs))
+    assert ref == cl.patterns()
 
+    # test write
+    cl.write(tmp_path / "test.emc")
+    assert ref == ef.patterns(tmp_path / "test.emc")
+
+    # test append with wrong size
     with pytest.raises(ValueError):
         cl.append(np.random.randint(32, size=35))
